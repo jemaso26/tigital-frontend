cbd8ad7de21509166b54de103d625f67edcf4443
Jay Christopher <jayanacio@gmail.com>
2023-08-08 18:41:32 +0800

    feat(app): implementing bootstrap css

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    // Handle form submission logic here
+  }
+}
